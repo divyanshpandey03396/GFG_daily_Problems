2f7afd9fc6a01a000276a72b752aa5bc980fa5d3
divyanshpandey03396 <divyanshpandey03396@gmail.com>
2022-06-08 13:44:14 +0530

    +1

---
+++ Nearly_sorted.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to return the sorted array.
+    vector <int> nearlySorted(int arr[], int num, int K){
+        // Your code here
+        vector<int>ans;
+        priority_queue<int,vector<int>,greater<int>>q;
+        for(int i=0;i<num;i++)
+        {
+            q.emplace(arr[i]);
+        }
+        for(int i=0;i<num;i++)
+        {
+            ans.push_back(q.top());
+            q.pop();
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+ {
+	int T;
+	cin>> T;
+	
+	while (T--)
+	{
+	    int num, K;
+	    cin>>num>>K;
+	    
+	    int arr[num];
+	    for(int i = 0; i<num; ++i){
+	        cin>>arr[i];
+	    }
+	    Solution ob;
+	    vector <int> res = ob.nearlySorted(arr, num, K);
+	    for (int i = 0; i < res.size (); i++)
+	        cout << res[i] << " ";
+	        
+	    cout<<endl;
+	}
+	
+	return 0;
+}
+  // } Driver Code Ends
