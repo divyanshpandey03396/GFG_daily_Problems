a795d1436e2afe1bfa277a57d36261c90829a500
divyanshpandey03396 <divyanshpandey03396@gmail.com>
2022-07-10 12:21:35 +0530

    +1

---
+++ All_possible_paths_from_top_to_bottom.cpp
@@ -0,0 +1,77 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+public:
+   vector<vector<int>> ans;
+    void solve(int i,int j,int n,int m,vector<vector<int>> &grid,vector<int> &temp){
+         if(i==n or j==m) return ;
+         temp.push_back(grid[i][j]);
+         if(i==n-1 and j==m-1) {
+             ans.push_back(temp);
+             return ;
+         }
+         if(j==m-1 and  i<n-1){
+             solve(i+1,j,n,m,grid,temp);
+             temp.pop_back();
+         }
+         if(i==n-1 and j<m-1){
+             solve(i,j+1,n,m,grid,temp);
+             temp.pop_back();
+         }
+         if(i<n-1 and j<m-1){
+             solve(i+1,j,n,m,grid,temp);
+             temp.pop_back();
+            solve(i,j+1,n,m,grid,temp);
+              temp.pop_back();
+         }
+       
+    }
+    vector<vector<int>> findAllPossiblePaths(int n, int m, vector<vector<int>> &grid)
+    {
+        vector<int> temp;
+        solve(0,0,n,m,grid,temp);
+       return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> grid(n, vector<int>(m));
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                cin >> grid[i][j];
+            }
+        }
+        Solution ob;
+        auto ans = ob.findAllPossiblePaths(n, m, grid);
+        for (int i = 0; i < ans.size(); i++)
+        {
+            for (int j = 0; j < ans[i].size(); j++)
+            {
+                cout << ans[i][j] << " ";
+            }
+            cout << "\n";
+        }
+    }
+    return 0;
+}
+  // } Driver Code Ends
